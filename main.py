e1400daede9f46fe8a9a21f363af0094c2f8a0cd
rgv14756-collab <rgv14756@gmail.com>
2026-03-15 15:29:16 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import telebot
+
+TOKEN = '8792226891:AAHq5cTfbdGVf7QPlugEAnN1WhES3t_jDa8'
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def say_hi(message):
+    bot.send_message(message.chat.id, "Бот запущен и работает!")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, f"Вы написали: {message.text}")
+
+if name == '__main__':
+    print("Бот пошел в работу...")
+    bot.infinity_polling()
